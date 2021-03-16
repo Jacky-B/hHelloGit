d8cf5c1c72299e95c0e32d161739e0cea0317c30
Jacky <jacquelineboina@gmail.com>
2021-03-16 13:40:05 +0100

    Initial Commit

---
+++ Shared/hHelloGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  hHelloGitApp.swift
+//  Shared
+//
+//  Created by jacky on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct hHelloGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
